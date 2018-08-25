4c80f06b4c44a24e17bdf5878b8b5462a93bf6b3
Unknown <jsim@fishbowlsolutions.com>
2018-08-25 08:19:50 +0100

    update dev config

--- config/index.js
+++ config/index.js
@@ -25,11 +25,37 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
+	host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
+	notifyOnErrors: true,
+	autoOpenBrowser: false,
+	errorOverlay: true,
+	poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+	// Use Eslint Loader?
+	// If true, your code will be linted during bundling and
+	// linting errors and warnings will be shown in the console.
+	useEslint: true,
+	// If true, eslint errors and warnings will also be shown in the error overlay
+	// in the browser.
+	showEslintErrorsInOverlay: false,
+
+	/**
+	 * Source Maps
+	 */
+
+	// https://webpack.js.org/configuration/devtool/#development
+	devtool: 'cheap-module-eval-source-map',
+
+	// If you have problems debugging vue-files in devtools,
+	// set this to false - it *may* help
+	// https://vue-loader.vuejs.org/en/options.html#cachebusting
+	cacheBusting: true,
+
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
