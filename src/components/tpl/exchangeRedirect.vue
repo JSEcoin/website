6b83ed8e075f80e22c50e5fe658747ced36b0b41
John Sim <jsim@fishbowlsolutions.com>
2018-10-21 20:28:53 +0100

    countdown redirect

--- src/components/tpl/exchangeRedirect.vue
+++ src/components/tpl/exchangeRedirect.vue
@@ -1,7 +1,8 @@
 <template>
-	<div>
-	  <h1 class="heading center">ICO Complete - 11/10/2018</h1>
-	  <h3 class="subHeading center">Redirecting you shortly to our exchange information page</h3>
+	<div style="margin-top:80px">
+		<h1 class="heading center">ICO Complete - 11/10/2018</h1>
+		<h3 class="subHeading center">Redirecting you shortly to our exchange information page</h3>
+		<h1 class="heading center">{{count}}</h1>
 	</div>
 </template>
 
@@ -10,18 +11,29 @@ export default {
 	name: 'exchangeRedirect',
 	data() {
 		return {
-			toggleItem: false,
+			count: 5,
 		};
 	},
 	mounted() {
 		const self = this;
-		setTimeout(() => {
+		setInterval(() => {
+			console.log('x');
+			if (self.count <= 1) {
+				self.redirect();
+			} else {
+				self.count--;
+			}
+		}, 1000);
+	},
+	methods: {
+		redirect() {
+			const self = this;
 			let rpath = '/exchanges';
 			if (typeof (self.$route.params.lang) !== 'undefined') {
 				rpath = `/${self.$route.params.lang}/exchanges`;
 			}
 			self.$router.push(rpath);
-		}, 5000);
+		},
 	},
 };
 </script>
